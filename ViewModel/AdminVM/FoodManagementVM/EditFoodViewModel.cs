5c4ae0b8a1f3be5122e2147103f6befc8378900c
22520650 <22520650@gm.uit.edu.vn>
2024-04-25 13:34:32 +0700

    fix lỗi giùm

---
+++ ViewModel/AdminVM/FoodManagementVM/EditFoodViewModel.cs
@@ -0,0 +1,85 @@
+﻿using cinema_management.DTOs;
+using cinema_management.Models.Services;
+using cinema_management.Utils;
+using cinema_management.Views;
+using cinema_management.Views.Admin.FoodManagement;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace cinema_management.ViewModel.AdminVM.AddFoodManagementVM
+{
+    public class EditManagementViewModel : BaseViewModel
+    {
+        public async Task LoadEditFood(EditFoodWindow wd)
+        {
+            if (SelectedItem != null)
+            {
+                DisplayName = SelectedItem.DisplayName;
+                wd._category.Text = SelectedItem.Category;
+                Price = SelectedItem.Price;
+                Image = SelectedItem.Image;
+                Id = SelectedItem.Id;
+                IsImageChanged = false;
+
+                ImageSource = await CloudinaryService.Ins.LoadImageFromURL(Image);
+            }
+        }
+
+        public async Task EditFood(Window p)
+        {
+            if (Id != -1 && IsValidData())
+            {
+                ProductDTO product = new ProductDTO();
+                product.DisplayName = DisplayName;
+                product.Category = Category.Content.ToString();
+                product.Price = Price;
+                product.Id = Id;
+                product.Quantity = Quantity;
+
+                if (IsImageChanged)
+                {
+                    if (Image != null)
+                    {
+                        await CloudinaryService.Ins.DeleteImage(Image);
+                    }
+
+                    product.Image = await Task.Run(() => CloudinaryService.Ins.UploadImage(filepath));
+
+                    if (product.Image is null)
+                    {
+                        MessageBoxCustom mb = new MessageBoxCustom("Thông báo", "Lỗi phát sinh trong quá trình lưu ảnh. Vui lòng thử lại", MessageType.Error, MessageButtons.OK);
+                        return;
+                    }
+                }
+                else
+                {
+                    product.Image = Image;
+                }
+
+                (bool successUpdateProduct, string messageFromUpdateProduct) = await ProductService.Ins.UpdateProduct(product);
+
+                if (successUpdateProduct)
+                {
+                    isSaving = false;
+                    LoadProductListView(Operation.UPDATE, product);
+                    MessageBoxCustom mb = new MessageBoxCustom("Thông báo", messageFromUpdateProduct, MessageType.Success, MessageButtons.OK);
+                    mb.ShowDialog();
+                    MaskName.Visibility = Visibility.Collapsed;
+                    p.Close();
+                }
+                else
+                {
+                    MessageBoxCustom mb = new MessageBoxCustom("Lỗi", messageFromUpdateProduct, MessageType.Error, MessageButtons.OK);
+                    mb.ShowDialog();
+                }
+
+            }
+            else
+            {
+                MessageBoxCustom mb = new MessageBoxCustom("Cảnh báo", "Vui lòng nhập đủ thông tin!", MessageType.Warning, MessageButtons.OK);
+                mb.ShowDialog();
+            }
+        }
+    }
+}
---
+++ ViewModel/AdminVM/FoodManagementVM/ImportFoodViewModel.cs
@@ -0,0 +1,55 @@
+﻿using cinema_management.DTOs;
+using cinema_management.Models.Services;
+using cinema_management.Utils;
+using cinema_management.Views;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cinema_management.ViewModel.AdminVM.FoodManagementVM
+{
+    public class FoodManagementViewModel : BaseViewModel
+    {
+        public void LoadImportFoodWindow(ImportFoodWindow wd)
+        {
+            SelectedProduct = null;
+            ImageSource = null;
+            Price = 0;
+            Quantity = 0;
+        }
+        public async Task ImportFood(Window p)
+        {
+            if (SelectedProduct != null)
+            {
+                if (Quantity > 0 && Price >= 0)
+                {
+                    ProductReceiptDTO productReceipt = new ProductReceiptDTO();
+                    productReceipt.ProductId = SelectedProduct.Id;
+                    productReceipt.ImportPrice = Price;
+                    productReceipt.Quantity = Quantity;
+                    productReceipt.StaffId = MainAdminViewModel.currentStaff.Id;
+
+                    (bool successAddProductReceipt, string messageFromAddProductReceipt, ProductReceiptDTO newProductReceipt) = await ProductReceiptService.Ins.CreateProductReceipt(productReceipt);
+
+                    if (successAddProductReceipt)
+                    {
+                        LoadProductListView(Operation.UPDATE_PROD_QUANTITY);
+                        MaskName.Visibility = Visibility.Collapsed;
+                        p.Close();
+                        MessageBoxCustom mb = new MessageBoxCustom("Thông báo", messageFromAddProductReceipt, MessageType.Success, MessageButtons.OK);
+                        mb.ShowDialog();
+                    }
+                }
+                else
+                {
+                    MessageBoxCustom mb = new MessageBoxCustom("Cảnh báo", "Số lượng hoặc giá nhập không hợp lệ!", MessageType.Warning, MessageButtons.OK);
+                    mb.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBoxCustom mb = new MessageBoxCustom("Cảnh báo", "Vui lòng chọn sản phẩm!", MessageType.Warning, MessageButtons.OK);
+                mb.ShowDialog();
+            }
+        }
+    }
+}
