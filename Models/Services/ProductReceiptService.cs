df20accb25d4201b8d75a1861c23e02ebb9835fa
Nguyen <vomainguyen120804@gmail.com>
2024-06-11 22:56:31 +0700

    finish

--- Models/Services/ProductReceiptService.cs
+++ Models/Services/ProductReceiptService.cs
@@ -43,7 +43,7 @@ namespace cinema_management.Models.Services
                                                  StaffId = pr.Staff.StaffID,
                                                  StaffName = pr.Staff.StaffName,
                                                  Quantity = pr.Quantity,
-                                                 ImportPrice = pr.ImportPrice,
+                                                 ImportPrice = pr.ImportPrice*pr.Quantity,
                                                  CreatedAt = pr.TimeOfReceipt,
                                              }).ToListAsync();
                 }
@@ -73,7 +73,7 @@ namespace cinema_management.Models.Services
                                                  StaffId = pr.Staff.StaffID,
                                                  StaffName = pr.Staff.StaffName,
                                                  Quantity = pr.Quantity,
-                                                 ImportPrice = pr.ImportPrice,
+                                                 ImportPrice = pr.ImportPrice * pr.Quantity,
                                                  CreatedAt = pr.TimeOfReceipt,
                                              }).ToListAsync();
                 }
